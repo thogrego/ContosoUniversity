1903418a02092094fadc16e03e8601189e9bf684
thogrego <thogrego@umich.edu>
2014-07-07 14:11:31 -0400

    Added column sort links to the students index page
    Added sorting functionality to the Index Method

--- ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -16,8 +16,27 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var students = from s in db.Students
+                           select s;
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
             return View(db.Students.ToList());
         }
 
